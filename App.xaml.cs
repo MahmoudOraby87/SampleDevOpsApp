fcc820bf6986dbbba9aeaa8eebf9035e580d5b0b
Mahmoud Orabi <mahmoudorabi@Mahmouds-MacBook-Pro.local>
2025-05-19 10:39:23 +0300

    Initial commit - Upload .NET MAUI project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SampleDevOpsApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SampleDevOpsApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
